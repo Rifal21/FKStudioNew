458611ec4e8ffaf65111dd8407d51e1adeca2a29
Rifal21 <giantjou77@gmail.com>
2026-05-08 04:49:31 +0700

    chore: make central_domains configurable via TENANCY_CENTRAL_DOMAINS environment variable

--- config/tenancy.php
+++ config/tenancy.php
@@ -16,10 +16,7 @@ return [
      *
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
-    'central_domains' => [
-        '127.0.0.1',
-        'localhost',
-    ],
+    'central_domains' => explode(',', env('TENANCY_CENTRAL_DOMAINS', 'localhost,127.0.0.1,fkstudio.id')),
 
     /**
      * Tenancy bootstrappers are executed when tenancy is initialized.
