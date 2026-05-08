2bb7ba739483aac67aab7b488a9e8dbc6935792e
Rifal21 <giantjou77@gmail.com>
2026-05-08 07:31:28 +0700

    feat: integrate multi-tenancy routing and middleware configuration in application bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,26 +1,46 @@
 <?php
 
+use App\Http\Middleware\EnsureTenantOnboarded;
+use App\Http\Middleware\RoleMiddleware;
 use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 return Application::configure(basePath: dirname(__DIR__))
+    // ->withRouting(
+    //     commands: __DIR__.'/../routes/console.php',
+    //     api: __DIR__.'/../routes/api.php',
+    //     health: '/up',
+    //     then: function () {
+    //         $centralDomains = config('tenancy.central_domains', []);
+    //         $currentHost = request()->getHost();
+    //         $isCentral = in_array($currentHost, $centralDomains);
+
+    //         if ($isCentral || empty($centralDomains)) {
+    //             Route::middleware('web')->group(base_path('routes/web.php'));
+    //         } else {
+    //             Route::middleware('web')->group(base_path('routes/tenant.php'));
+    //         }
+    //     }
+    // )
     ->withRouting(
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            $centralDomains = config('tenancy.central_domains', []);
-            $currentHost = request()->getHost();
-            $isCentral = in_array($currentHost, $centralDomains);
 
-            if ($isCentral || empty($centralDomains)) {
-                Route::middleware('web')->group(base_path('routes/web.php'));
-            } else {
-                Route::middleware('web')->group(base_path('routes/tenant.php'));
-            }
-        }
+            Route::middleware([
+                'web',
+                InitializeTenancyByDomain::class,
+                PreventAccessFromCentralDomains::class,
+            ])->group(base_path('routes/tenant.php'));
+
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
@@ -28,8 +48,8 @@ return Application::configure(basePath: dirname(__DIR__))
             SetLocale::class,
         ]);
         $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
-            'tenant.onboarded' => \App\Http\Middleware\EnsureTenantOnboarded::class,
+            'role' => RoleMiddleware::class,
+            'tenant.onboarded' => EnsureTenantOnboarded::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
